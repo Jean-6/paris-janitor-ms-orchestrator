095edb7dccbccb627fa0e83f1bad10b0512b7e9c
Jean <oka.jeanjaures@gmail.com>
2025-07-24 03:18:16 +0200

    initial commit

---
+++ src/main/java/org/example/parisjanitormsorchestrator/ParisJanitorMsOrchestratorApplication.java
@@ -0,0 +1,13 @@
+package org.example.parisjanitormsorchestrator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParisJanitorMsOrchestratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParisJanitorMsOrchestratorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/parisjanitormsorchestrator/ParisJanitorMsOrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.parisjanitormsorchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParisJanitorMsOrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
